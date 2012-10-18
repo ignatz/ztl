9a570a66899fa6249ab21e82051bfbd2e6adf005
Sebastian Jeltsch <sjeltsch@kip.uni-heidelberg.de>
2012-10-18 13:17:05 +0200

    drastically simplify serialization

--- ztl/array/serialize.h
+++ ztl/array/serialize.h
@@ -5,6 +5,7 @@
 
 #include <boost/serialization/serialization.hpp>
 #include <boost/serialization/nvp.hpp>
+#include <boost/serialization/array.hpp>
 
 #include "ztl/array/impl.h"
 #include "ztl/array/trait.h"
@@ -16,7 +17,7 @@ namespace serialization {
 // ArrayInterface serializer
 template<typename Archiver, template<typename, size_t, size_t, typename...> class Class,
 typename T, size_t N, typename ... Args>
-void serialize(Archiver & ar, ZTL::ArrayInterface<Class<T, N, 0, Args...>, N> & s,
+void serialize(Archiver& ar, ZTL::ArrayInterface<Class<T, N, 0, Args...>, N>& s,
 	unsigned int const)
 {
 	using namespace boost::serialization;
@@ -24,38 +25,23 @@ void serialize(Archiver & ar, ZTL::ArrayInterface<Class<T, N, 0, Args...>, N> & 
 }
 
 
-
-// base class BaseArray serializer
-template<typename Archiver, typename T, size_t N, size_t Idx>
-void serialize(Archiver & ar, ZTL::BaseArray<T, N, Idx>& s, unsigned int const)
-{
-	using namespace boost::serialization;
-	ar & make_nvp("value", s.value);
-}
-
-
-
 // serializers for all implementations
 template<typename Archiver, template<typename, size_t, size_t, typename...> class Class,
 	typename T, size_t N, size_t Idx, typename ... Args, typename = typename ZTL::enable_if<
 		ZTL::is_array_impl<Class<T, N, Idx, Args...>>::value>::type>
 typename ZTL::enable_if<(Idx+1<N), void>::type
-serialize(Archiver & ar, Class<T, N, Idx, Args...>& s, unsigned int const)
+serialize(Archiver& ar, Class<T, N, Idx, Args...>& s, unsigned int const)
 {
 	using namespace boost::serialization;
-	ar & make_nvp("base", base_object<ZTL::BaseArray<T, N, Idx>>(s))
-	   & make_nvp("next", s.next);
+	ar & make_nvp("data", reinterpret_cast<boost::array<T, N>&>(s));
 }
 
 template<typename Archiver, template<typename, size_t, size_t, typename...> class Class,
 	typename T, size_t N, size_t Idx, typename ... Args, typename = typename ZTL::enable_if<
 		ZTL::is_array_impl<Class<T, N, Idx, Args...>>::value>::type>
 typename ZTL::enable_if<(Idx+1>=N), void>::type
-serialize(Archiver & ar, Class<T, N, Idx, Args...>& s, unsigned int const)
-{
-	using namespace boost::serialization;
-	ar & make_nvp("base", base_object<ZTL::BaseArray<T, N, Idx>>(s));
-}
+serialize(Archiver&, Class<T, N, Idx, Args...>&, unsigned int const)
+{}
 
 } // namespace serialization
 } // namespace boost
