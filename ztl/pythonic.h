fe7fac0859d87bdb025ee9b1ed6e6347ad0135e1
Sebastian Jeltsch <sjeltsch@kip.uni-heidelberg.de>
2013-02-12 13:31:32 +0100

    remove pythonic. From now on use standalone version (https://github.com/ignatz/pythonic)
    was no 'real' TMP anyway ;)

--- ztl/pythonic.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Copyright (c) 2013, Sebastian Jeltsch (sjeltsch@kip.uni-heidelberg.de)
-// Distributed under the terms of the GPLv2 or newer
-
-#include "ztl/pythonic/enumerate.h"
-#include "ztl/pythonic/zip.h"
